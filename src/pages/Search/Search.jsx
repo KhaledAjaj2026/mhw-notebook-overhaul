e021fe48bcd824d032939ff38978e45c0fc75c04
Khaled Ajaj <khaledajaj2026@gmail.com>
2024-01-10 11:04:11 -0800

    Feat: animate search-bar rise and fall on submit
    Search bar rises when submit button is pressed. This allows room for
    search result to appear below while keeping the search bar at top. Main
    header disappears indefinitely until Search page refreshed or when
    Search page visited from another page.

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -10,47 +10,51 @@ export default function Search() {
 	const [result, setResult] = new useState(true);
 
 	return (
-		<div id='page_search-container'>
-			<NavBar />
-			<header
-				className={`main-header_container${
-					result ? ' fade-appear' : ' fade-disappear'
-				}`}
-			>
-				<h1 className='main-header_title'>
-					Start your
-					<br />
-					adventure
-				</h1>
-				<img className='main-header_flare' src={flare} alt='lens flare' />
-			</header>
-			<form className={`${result ? 'search_input-fall' : 'search_input-rise'}`}>
-				<label htmlFor='monster-search' className='search_label'>
-					Search for a monster below
-				</label>
-				<div className='search_input__icon'>
-					<input
-						type='text'
-						name='monster-search'
-						id='monster-search'
-						placeholder='E.g. Anjanath'
-					/>
-					<button
-						type='button'
-						className='search_submit'
-						onClick={() => setResult(!result)}
-					>
-						<img
-							src='src/assets/misc/magnifying-glass-9.svg'
-							className='search_icon'
-							alt='magnifying glass'
+		<>
+			<div id='page_search-container'>
+				<NavBar />
+				<header
+					className={`main-header_container${
+						result ? ' fade-appear' : ' fade-disappear'
+					}`}
+				>
+					<h1 className='main-header_title'>
+						Start your
+						<br />
+						adventure
+					</h1>
+					<img className='main-header_flare' src={flare} alt='lens flare' />
+				</header>
+				<form
+					className={`${result ? 'search_input-fall' : 'search_input-rise'}`}
+				>
+					<label htmlFor='monster-search' className='search_label'>
+						Search for a monster below
+					</label>
+					<div className='search_input__icon'>
+						<input
+							type='text'
+							name='monster-search'
+							id='monster-search'
+							placeholder='E.g. Anjanath'
 						/>
-					</button>
-				</div>
-				{/* <Autofill /> */}
-				<Result />
-			</form>
+						<button
+							type='button'
+							className='search_submit'
+							onClick={() => setResult(false)}
+						>
+							<img
+								src='src/assets/misc/magnifying-glass-9.svg'
+								className='search_icon'
+								alt='magnifying glass'
+							/>
+						</button>
+					</div>
+					{/* <Autofill /> */}
+					<Result />
+				</form>
+			</div>
 			<Footer />
-		</div>
+		</>
 	);
 }
