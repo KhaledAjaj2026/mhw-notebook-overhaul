591c715a3f15710d65cdb51670cb07a505ab0a0e
Khaled Ajaj <khaledajaj2026@gmail.com>
2024-01-16 15:50:12 -0800

    Feat: Animate on load, animate flip, responsive
    Contain leather-cover element within new parent div for responsive
    background-image size according to viewport width and height. Animate
    (keyframe) initial loading by having book slide up into view, only to play upon
    loading site. Animate (transition) page flip with proper perspective
    set from the user's viewpoint. Set state for handling flip animation.
    Change nav link to button that changes state of flip. Comment out
    fade-in/fade-out animations from 'index.scss' file temporarily.

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,32 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Book.css';
 
 export default function Book() {
-	useEffect(() => {
-		window.onload = console.log('jsaflkdsaj');
-	});
+	const [open, setOpen] = new useState(false);
+
+	const handleOpen = () => {
+		setOpen(!open);
+	};
+	// useEffect(() => {
+	// 	window.onload = console.log('jsaflkdsaj');
+	// });
 	return (
-		<>
+		<div className='book-container'>
 			<div id='background'></div>
-			<div id='book-container'>
-				<div className='book-leather_cover'>
-					<header>
-						<h1 className='cover-header_title'>
-							Monster Hunter World
-							<br />
-							<span className='cover-header_title-subheader'>
-								Hunter's Notes
-							</span>
-						</h1>
-					</header>
-					<div className='cover-description'>
-						<p>A guide to monster ecology, physiology, and rewards</p>
-					</div>
-					<a href='/' className='navigation-button'>
-						Search Now
-					</a>
+			<div className={`book-leather_cover slide-up${open ? ' flip-open' : ''}`}>
+				<header>
+					<h1 className='cover-header_title'>
+						Monster Hunter World
+						<br />
+						<span className='cover-header_title-subheader'>Hunter's Notes</span>
+					</h1>
+				</header>
+				<div className='cover-description'>
+					<p>A guide to monster ecology, physiology, and rewards</p>
 				</div>
+				<button className='navigation-button' onClick={handleOpen}>
+					Search Now
+				</button>
 			</div>
-		</>
+		</div>
 	);
 }
