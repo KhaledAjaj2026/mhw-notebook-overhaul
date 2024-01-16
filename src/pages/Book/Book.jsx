3ce88c7545a7c13be5d8d1986bc2648c32175273
Khaled Ajaj <khaledajaj2026@gmail.com>
2024-01-16 12:51:25 -0800

    Feat: build book cover, set background image
    Background image set to wooden surface to resemble desktop. Book cover
    is leather binding, image set as background. Both images have soft
    linear gradients to simulate lamp light. Set golden gradient and clip
    to text. Use placeholder button, will replace with inset gem image.

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import flare from '../../assets/misc/lens-flare-green.png';
-import Search from '../../components/Search/Search';
+import React, { useEffect } from 'react';
 import './Book.css';
 
 export default function Book() {
+	useEffect(() => {
+		window.onload = console.log('jsaflkdsaj');
+	});
 	return (
-		<div id='book-container'>
-			<header className='main-header_container'>
-				<h1 className='main-header_title'>Discover Monsters</h1>
-				<img className='main-header_flare' src={flare} alt='lens flare' />
-			</header>
-			<Search />
-			<div className='main-description'>
-				<p>
-					Learn about monster ecology, hunting techniques, quest rewards, and
-					more
-				</p>
+		<>
+			<div id='background'></div>
+			<div id='book-container'>
+				<div className='book-leather_cover'>
+					<header>
+						<h1 className='cover-header_title'>
+							Monster Hunter World
+							<br />
+							<span className='cover-header_title-subheader'>
+								Hunter's Notes
+							</span>
+						</h1>
+					</header>
+					<div className='cover-description'>
+						<p>A guide to monster ecology, physiology, and rewards</p>
+					</div>
+					<a href='/' className='navigation-button'>
+						Search Now
+					</a>
+				</div>
 			</div>
-			<a href='/' className='navigation-button'>
-				Search Now
-			</a>
-		</div>
+		</>
 	);
 }
