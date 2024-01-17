c8e16351c2b9d872f1c1cd15fc4754387ae89786
Khaled Ajaj <khaledajaj2026@gmail.com>
2024-01-17 12:05:43 -0800

    Feat: add Leaf title, flip page wider
    Add title to Leaf component, atop search component. Title is responsive.
    Page-flip animation opens wider, creates better illusion of opening
    book on top of a desk. Adjust styling of elements to fit within page,
    such as margins.

--- src/pages/Leaf/Leaf.jsx
+++ src/pages/Leaf/Leaf.jsx
@@ -5,6 +5,11 @@ import './Leaf.css';
 function Leaf({ open }) {
 	return (
 		<div className={`book-leaf${open ? '' : ' hidden'}`}>
+			<h2 className='book-leaf_title'>
+				<span className='book-leaf_title-top'>Discover the</span>
+				<br />
+				New World
+			</h2>
 			<Search />
 		</div>
 	);
