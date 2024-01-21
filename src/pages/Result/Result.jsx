0fe3654a4fcc883d9af61a98d1ab8bf59bceb162
Khaled Ajaj <khaledajaj2026@gmail.com>
2024-01-20 17:07:37 -0800

    Feat: delete unused assets, style Result page 1
    Delete assets used by previous version (tag v1.0) of this project. Style
    Result component first page; show name, image, and description of
    monster. Use Anjanath info as placeholder data.

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import anjanath from '../../assets/monsters-min/anjanath.webp';
-// import border from '../../assets/misc/fancy-border.png';
+import rajang from '../../assets/monsters-min/rajang.webp';
 import rightCorner from '../../assets/misc/gold_corner-bottom_right.png';
 import leftCorner from '../../assets/misc/gold_corner-bottom_left.png';
 import './Result.css';
@@ -12,9 +12,24 @@ export default function Result(
 	turnAndSeek,
 	undoTurnAndSeek
 ) {
+	const monster = 'anjanath';
+
 	return (
 		<>
 			<h2 className='result-leaf_title'>Anjanath</h2>
+			<div className='result-image_container'>
+				{/* <img src={border} alt='fancy border' className='result-image_border' /> */}
+				<img
+					src={anjanath}
+					className='result-monster_image'
+					alt={`image of ${monster}`}
+				/>
+			</div>
+			<p className='result-monster_description'>
+				The Anjanath patrols the Ancient Forest, looking for its favorite meal,
+				Aptonoth. This belligerent monster will attack anything without
+				hesitation.
+			</p>
 			<button type='button' className='result-turn_leaf-button-right'>
 				<img src={rightCorner} alt='decorative corner' />
 			</button>
