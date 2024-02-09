e33be1a71118469507a4a6d30a8cd24de599050d
Khaled Ajaj <khaledajaj2026@gmail.com>
2024-02-09 13:30:49 -0800

    Feat: darken surrounding area when focus on Search
    Darken area around Search bar to guide user towards search bar. Dark
    area disappears when unfocused, or 'onBlur'. Use global opacity
    transition animation for making dark area appear/disappear.

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 
 export default function Search({
@@ -9,6 +9,9 @@ export default function Search({
 	handleResult,
 	disabled,
 }) {
+	const [focus, setFocus] = new useState(false);
+	const handleFocus = () => setFocus(!focus);
+
 	const checkInput = () => {
 		if (input.length > 0) {
 			handleResult();
@@ -18,32 +21,40 @@ export default function Search({
 	};
 
 	return (
-		<form
-			className={`${showSearch ? 'appear' : 'disappear'}${
-				result ? ' raise_search' : ''
-			}`}
-		>
-			<label htmlFor='monster-search' className='search_label'>
-				Search for a monster below
-			</label>
-			<div className='search_input__icon'>
-				<input
-					type='text'
-					name='monster-search'
-					id='monster-search'
-					placeholder='E.g. Anjanath'
-					disabled={disabled}
-					onChange={handleInput}
-				/>
-				<button type='button' className='search_submit' onClick={checkInput}>
-					<img
-						src='src/assets/misc/magnifying-glass-9.svg'
-						className='search_icon'
-						alt='magnifying glass'
+		<>
+			<div
+				className={`dark_background${focus ? ' appear' : ' disappear'}`}
+			></div>
+			<form
+				className={`${showSearch ? 'appear' : 'disappear'}${
+					result ? ' raise_search' : ''
+				}`}
+			>
+				<label htmlFor='monster-search' className='search_label'>
+					Search for a monster below
+				</label>
+				<div className='search_input__icon'>
+					<input
+						type='text'
+						name='monster-search'
+						id='monster-search'
+						placeholder='E.g. Anjanath'
+						autoComplete='off'
+						disabled={disabled}
+						onChange={handleInput}
+						onFocus={handleFocus}
+						onBlur={handleFocus}
 					/>
-				</button>
-			</div>
-			{/* <Autofill /> */}
-		</form>
+					<button type='button' className='search_submit' onClick={checkInput}>
+						<img
+							src='src/assets/misc/magnifying-glass-9.svg'
+							className='search_icon'
+							alt='magnifying glass'
+						/>
+					</button>
+				</div>
+				{/* <Autofill /> */}
+			</form>
+		</>
 	);
 }
